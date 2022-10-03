b7d0bd8c7d260f911815691162217d027e2cb59d
alsu Bulatova <bulatovaalsu22@gmail.com>
2022-10-04 01:05:14 +0300

    setting css modules

---
+++ src/Form/Form.js
@@ -0,0 +1,12 @@
+function Form() {
+    return (
+        <div id="form">
+        <label class="input">
+          <input id="input" type="text" placeholder="Введите свое имя"></input>
+          <button onclick="start()">START</button>
+        </label>
+    </div>
+    );
+  }
+  
+  export default Form;
---
+++ src/Game/Canvas.js
@@ -0,0 +1,7 @@
+function Canvas() {
+    return (
+        <><canvas width="608" height="608" id="game"></canvas><div id="score"></div></>
+    );
+  }
+  
+  export default Canvas;
